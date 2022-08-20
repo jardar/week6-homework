e921b7e81bd0bc0ba81d9ca2fbc8550862168d47
JarDar <jardar.tsai@gmail.com>
2022-08-20 21:03:46 +0800

    home page - mobile

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,54 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+
+  var myCourseswiper = new Swiper(".myCourseSwiper", {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      570: {
+        // slidesPerView: 3,
+        spaceBetween: 8
+      },
+      // when window width is >= 480px
+      768: {
+        // slidesPerView: 4,
+        spaceBetween: 16
+      },
+      // when window width is >= 640px
+      992: {
+        // slidesPerView: 5,
+        spaceBetween: 16
+      }
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var myReviewSwiper = new Swiper(".myReviewSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    cssMode: true,
+    navigation: {
+      nextEl: ".review-swiper-button-next",
+      prevEl: ".review-swiper-button-prev",
+    },
+    pagination: {
+      el: ".review-swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
 });
+
+
